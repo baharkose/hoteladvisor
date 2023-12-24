c332f9cb0c60107916827041a0a01f907b03cca1
baharkose <baharkse17@gmail.com>
2023-12-24 14:05:54 +0300

    extend colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary:"#AC87C5",
+        //- burada exted ettiğimizi tailwind classı gibi kullanabiliyoruz. 
+      }
+    },
   },
   plugins: [],
   darkMode: 'class',
